3c680d3a0017625939bdf11150a7f8285100ebf8
Vital-Minsk <70841159+Vital-Minsk@users.noreply.github.com>
2021-08-11 13:45:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+System.Console.WriteLine("Enter your name!");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello {name},what is your last name?");
+string name2 = Console.ReadLine();
+Console.WriteLine("The last question, were you born?");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{name2} {name} to you now {DateTime.Now.Year - age} year");
